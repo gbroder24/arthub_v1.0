5e65caccaa1f2997e3d94f5d9afad25f6d089b50
gbroder24 <garybroderick82@gmail.com>
2025-02-18 10:09:35 +0000

    Create procfile, runtime text file and update settings and requirements

--- arthub/settings.py
+++ arthub/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 import os
+import dj_database_url
 from pathlib import Path
 
 if os.path.isfile('env.py'):
@@ -24,16 +25,18 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = (
-    'django-insecure-#6=hpu5*i=e=z@d_e-s_bkit%v25^71+v4@cg1j2c(xsd4g5tl'
-)
+SECRET_KEY = (os.environ.get('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
-    '127.0.0.1',  # vs code preview
-    'localhost',  # listen for stripe webhooks
+    # vs code preview
+    '127.0.0.1',
+    # listen for stripe webhooks
+    'localhost',
+    # heroku application
+    'django-pp5-arthub-37c0b88f7fd7.herokuapp.com/',
 ]
 
 
@@ -135,13 +138,17 @@ WSGI_APPLICATION = 'arthub.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
-
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
